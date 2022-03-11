9ea88ac85238cef34626f24e5962ebc0e02e3be8
jethass <jethass@hotmail.com>
2022-03-11 12:06:14 +0100

    Create Cache.php

---
+++ App/Cache.php
@@ -0,0 +1,20 @@
+<?php
+Namespace App;
+
+class Cache implements CacheInterface {
+
+    public function get($key)
+    {
+        return false;
+    }
+
+    public function has($key)
+    {
+        return false;
+    }
+
+    public function set($key, $value, $expiration = 3600)
+    {
+        return false;
+    }
+}
