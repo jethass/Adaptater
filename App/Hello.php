02e0590c0e623043abaeae0686229e1307fb35c7
jethass <jethass@hotmail.com>
2022-03-11 11:58:36 +0100

    Update Hello.php

--- App/Hello.php
+++ App/Hello.php
@@ -1,1 +1,19 @@
+<?php
+Namespace App;
 
+class Hello
+{
+
+    public function sayHello(CacheInterface $cache)
+    {
+        if($cache->has('hello')) {
+            return $cache->get('hello');
+        } else {
+            sleep(4); // On simule un script lent 
+            $content = 'bonjour';
+            $cache->set('hello', $content);
+            return $content;
+        }
+    }
+
+}
